e2b91424b442210bac76c13cc00843547ec72c6c
Lawrence_Yang <60809993+TOBEACODER7@users.noreply.github.com>
2023-09-05 17:06:02 +0800

    add addnum

---
+++ traffic/addnum/target/debug/deps/addnum-77432d7ff4728451.d
@@ -0,0 +1,5 @@
+/home/lawrencee/rust/Rust-test/addnum/target/debug/deps/addnum-77432d7ff4728451.rmeta: src/main.rs
+
+/home/lawrencee/rust/Rust-test/addnum/target/debug/deps/addnum-77432d7ff4728451.d: src/main.rs
+
+src/main.rs:
---
+++ traffic/addnum/target/debug/deps/addnum-7c02327903422be7.d
@@ -0,0 +1,5 @@
+/home/lawrencee/rust/Rust-test/addnum/target/debug/deps/addnum-7c02327903422be7.rmeta: src/main.rs
+
+/home/lawrencee/rust/Rust-test/addnum/target/debug/deps/addnum-7c02327903422be7.d: src/main.rs
+
+src/main.rs:
